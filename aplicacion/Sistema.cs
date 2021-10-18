587c74ab2ba97b2d45f96c8b1b875490b18dcbb5
AsierBreyas <72077655+AsierBreyas@users.noreply.github.com>
2021-10-18 11:54:58 +0200

    Completada la Vista

--- aplicacion/Sistema.cs
+++ aplicacion/Sistema.cs
@@ -1,4 +1,13 @@
+using System.Collections.Generic;
 class Sistema
 {
-    
+    //Datos
+    List<int> data = new();
+    //Metodos de en lenguaje empresarial 
+
+    int Metodo1( int param1, int param2){
+        if(param1 > 7) return -3;
+
+        return param1 + param2;
+    }
 }
--- presentacion/Controlador.cs
+++ presentacion/Controlador.cs
@@ -3,8 +3,18 @@ class Controlador
     private Sistema _sistema;
     private Vista _vista;
 
-    public Controlador(Sistema sistema, Vista vista){
+    public Controlador(Sistema sistema, Vista vista)
+    {
         _sistema = sistema;
         _vista = vista;
+    }
+
+    void CasoDeIso1()
+    {
+
+    }
+    void CasoDeUso2()
+    {
+
     }
 }
